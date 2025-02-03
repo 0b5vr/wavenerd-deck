8474dab24d47ff13beb843b5dc4565464a9535f6
0b5vr <0b5vr@0b5vr.com>
2025-02-04 00:19:14 +0900

    deps: Bump eslint to latest
    used wavenerd repo's config

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      '**/docs/**/*',
+      '**/dist/**/*',
+      '**/types/**/*',
+    ],
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs.customize({
+    semi: true,
+    braceStyle: '1tbs',
+    arrowParens: true,
+    quoteProps: 'consistent',
+  }),
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@stylistic/max-statements-per-line': ['error', { max: 2 }],
+    },
+  },
+];
