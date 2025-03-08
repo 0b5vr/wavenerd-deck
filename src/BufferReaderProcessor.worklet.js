5c225a645a72214d7bb636d9bc7744d8be23e170
0b5vr <0b5vr@0b5vr.com>
2025-03-09 02:28:25 +0900

    refactor: Refactor BufferReaderProcessor

--- src/BufferReaderProcessor.worklet.js
+++ src/BufferReaderProcessor.worklet.js
@@ -10,49 +10,34 @@ class BufferReaderProcessor extends AudioWorkletProcessor {
     super();
 
     this.active = false;
-    this.buffer = new Float32Array( CHANNELS * FRAMES_PER_CHANNEL );
+    this.buffer = new Float32Array(CHANNELS * FRAMES_PER_CHANNEL);
     this.blocks = 0;
     this.written = 0;
     this.underrun = false;
 
-    this.port.onmessage = ( { data } ) => {
-      if ( Array.isArray( data ) ) {
+    this.port.onmessage = ({ data }) => {
+      if (Array.isArray(data)) {
         const [channel, block, buffer] = data;
-        const frame = (block % BLOCKS_PER_CHANNEL) * BLOCK_SIZE;
-        const chHead = FRAMES_PER_CHANNEL * channel;
-
-        // Check if we need to handle wrap-around
-        if (frame + buffer.length <= FRAMES_PER_CHANNEL) {
-          // No wrap-around needed
-          this.buffer.set( buffer, chHead + frame );
-        } else {
-          // Need to handle wrap-around
-          const firstPartFrames = FRAMES_PER_CHANNEL - frame;
-
-          this.buffer.set( buffer.subarray(0, firstPartFrames), chHead + frame ); // the first part
-          this.buffer.set( buffer.subarray(firstPartFrames), chHead ); // the second part
-        }
-
-        this.written = block + buffer.length / BLOCK_SIZE;
+        this.storeBuffer(channel, block, buffer);
       } else {
         this.active = data;
       }
     };
   }
 
-  process( inputs, outputs, parameters ) {
-    if ( this.active ) {
+  process(inputs, outputs, parameters) {
+    if (this.active) {
       const underrun = this.written <= this.blocks;
-      this.setUnderrun( underrun );
+      this.setUnderrun(underrun);
 
-      if ( !underrun ) {
+      if (!underrun) {
         const buffer = this.buffer;
         const frame = (this.blocks * BLOCK_SIZE) % FRAMES_PER_CHANNEL;
 
-        outputs[ 0 ].forEach( ( ch, iCh ) => {
+        outputs[0].forEach((ch, iCh) => {
           const chHead = FRAMES_PER_CHANNEL * iCh;
-          ch.set( buffer.subarray( chHead + frame, chHead + frame + BLOCK_SIZE ) );
-        } );
+          ch.set(buffer.subarray(chHead + frame, chHead + frame + BLOCK_SIZE));
+        });
       }
     }
 
@@ -61,12 +46,31 @@ class BufferReaderProcessor extends AudioWorkletProcessor {
     return true;
   }
 
-  setUnderrun( underrun ) {
-    if ( this.underrun !== underrun ) {
+  storeBuffer(channel, block, buffer) {
+    const frame = (block % BLOCKS_PER_CHANNEL) * BLOCK_SIZE;
+    const chHead = FRAMES_PER_CHANNEL * channel;
+
+    // Check if we need to handle wrap-around
+    if (frame + buffer.length <= FRAMES_PER_CHANNEL) {
+      // No wrap-around needed
+      this.buffer.set(buffer, chHead + frame);
+    } else {
+      // Need to handle wrap-around
+      const firstPartFrames = FRAMES_PER_CHANNEL - frame;
+
+      this.buffer.set(buffer.subarray(0, firstPartFrames), chHead + frame); // the first part
+      this.buffer.set(buffer.subarray(firstPartFrames), chHead); // the second part
+    }
+
+    this.written = block + buffer.length / BLOCK_SIZE;
+  }
+
+  setUnderrun(underrun) {
+    if (this.underrun !== underrun) {
       this.underrun = underrun;
-      this.port.postMessage( underrun );
+      this.port.postMessage(underrun);
     }
   }
 }
 
-registerProcessor( 'buffer-reader-processor', BufferReaderProcessor );
+registerProcessor('buffer-reader-processor', BufferReaderProcessor);
